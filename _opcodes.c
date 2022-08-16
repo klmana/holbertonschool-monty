e3779d8f7e36d44a9ff57a029e097d7e3ee82709
Gotmished <5002@holbertonschool.com>
2022-08-15 20:46:59 -0700

    Initial commit - function to implement opcodes

---
+++ _opcodes.c
@@ -0,0 +1,37 @@
+#include <string.h>
+#include "monty.h"
+
+/**
+ * _opcodes - executes each line of a Monty file
+ * @code: the name of the opcode to check and use
+ * @stack: the stack on which the opcode will be executed
+ * @count: the line number in the Monty file containing an opcode
+ */
+
+void _opcodes(char *code, stack_t **stack, unsigned int count)
+{
+	int i;
+	instruction_t opcodes[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{NULL, NULL}
+	};
+
+	i = 0;
+	while (opcodes[i].opcode != NULL)
+	{
+		if (strcmp(opcodes[i].opcode, code) == 0)
+		{
+			opcodes[i].f(stack, count);
+			return;
+		}
+		i = i + 1;
+	}
+	fprintf(stderr, "L%d: unknown instruction %s\n", count, code);
+	exit(1);
+}
